27486fb5397b6c8f672e5fbf29dfd8810b29f8e8
naluthge <niroshaluthge@gmail.com>
2018-08-07 16:52:26 -0500

    Add existing file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" = "" ]; then
+	printf "\nProvide a link for USEARCH download (from email) as argument.\nGet a license from http://www.drive5.com/usearch/download.html\nSee RMarkdown file for details.\n\n"
+    exit 1
+fi
+
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-MacOSX-x86_64.sh
+bash Anaconda-2.3.0-MacOSX-x86_64.sh
+
+anaconda/bin/conda create -n STECEnv python pip numpy matplotlib=1.4.3 scipy pandas cython mock nose
+source anaconda/bin/activate STECEnv
+pip install https://github.com/biocore/qiime/archive/1.9.0.tar.gz
+
+anaconda/bin/conda install -c r r r-xml
+anaconda/bin/conda install -c https://conda.binstar.org/asmeurer pandoc
+anaconda/bin/conda install -c https://conda.binstar.org/r rpy2=2.5.6
+
+wget -O anaconda/envs/rumenEnv/bin/usearch $1
+chmod 775 anaconda/envs/rumenEnv/bin/usearch 
